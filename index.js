9e9c7f4bfb2fcb37367f04305e32fda86ad38040
Amirali Mansouri <mansouri.amirali98@gmail.com>
2023-02-02 03:39:13 +0330

    Create User model and dummy user and store in req

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const userRoutes = require('./routes/user');
 
 const sequelize = require('./utils/database');
 
+const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,12 +19,34 @@ app.use(express.static(path.join('public')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(userRoutes);
 
 sequelize
   .sync()
+  // .sync({ force: true })
   .then(() => {
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({
+        fullName: 'Amirali Mansouri',
+        email: 'mansouri.amirali98@gmail.com',
+      });
+    }
+    return user;
+  })
+  .then(user => {
     app.listen(3000);
   })
   .catch(err => console.log(err));
---
+++ models/user.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/database');
+
+const User = sequelize.define('user', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  email: DataTypes.STRING,
+  fullName: DataTypes.STRING,
+});
+
+module.exports = User;
